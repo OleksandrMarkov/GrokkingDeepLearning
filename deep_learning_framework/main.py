aee7a1b02fe4405907c99f03ff7b479e724f7cbe
Oleksandr Markov <ol.markov1999@gmail.com>
2022-09-20 20:37:07 +0300

    applying tanh and sigmoid in network

--- deep_learning_framework/main.py
+++ deep_learning_framework/main.py
@@ -2,6 +2,11 @@ from package.tensor import Tensor
 from package.SGD import SGD
 from package.linear import Linear
 from package.sequential import Sequential
+from package.MSELoss import MSELoss
+
+from package.tanh import Tanh
+from package.sigmoid import Sigmoid
+
 import numpy as np
 np.random.seed(0)
 
@@ -9,16 +14,17 @@ if __name__ == '__main__':
     data = Tensor(np.array([[0,0],[0,1],[1,0],[1,1]]), autograd = True)
     target = Tensor(np.array([[0],[1],[0],[1]]), autograd = True)
 
-    model = Sequential([Linear(2,3), Linear(3,1)])
-    
-    optim = SGD(parameters = model.get_parameters(), alpha = 0.05)
+    model = Sequential([Linear(2,3), Tanh(), Linear(3,1), Sigmoid()])
+    criterion = MSELoss()    
+        
+    optim = SGD(parameters = model.get_parameters(), alpha = 1)
 
     for i in range(10):
         # Predict
         pred = model.forward(data)
         
         # Compare
-        loss = ((pred - target)*(pred - target)).sum(0)
+        loss = criterion.forward(pred, target)
         
         # Learn
         loss.backward(Tensor(np.ones_like(loss.data)))
