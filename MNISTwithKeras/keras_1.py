87439685c272452ab3771a0ad153f604e5034f9c
Oleksandr Markov <ol.markov1999@gmail.com>
2022-09-28 19:42:48 +0300

    dimensions and type of images data

--- MNISTwithKeras/keras_1.py
+++ MNISTwithKeras/keras_1.py
@@ -2,6 +2,9 @@ from keras.datasets import mnist
 
 (train_images, train_labels), (test_images, test_labels) = mnist.load_data()
 
-print(train_images.shape) # 60000 28 28
-print(test_images.shape) # 10000 28 28
+print(train_images.shape) # array 60000x28x28
+print(train_images.ndim) # 3
+print(train_images.dtype) #uint8
+
+print(test_images.shape) # array 10000x28x28
 print(len(test_labels)) # 10000
