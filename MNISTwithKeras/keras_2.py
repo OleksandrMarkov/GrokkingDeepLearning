ed4db002f864b6844d7ba0fd5713b67c13be6fb4
Oleksandr Markov <ol.markov1999@gmail.com>
2022-09-28 19:01:50 +0300

    Simple neural network for classifying handwritten digits

---
+++ MNISTwithKeras/keras_2.py
@@ -0,0 +1,24 @@
+from keras.datasets import mnist
+(train_images, train_labels), (test_images, test_labels) = mnist.load_data()
+
+from keras import models, layers
+
+model = models.Sequential([
+	layers.Dense(512, activation = 'relu'), # first layer
+	# 10-переменный слой потерь, возвращает вероятности принадлежности изображения к цифрам
+	layers.Dense(10, activation = 'softmax') # second layer
+])
+
+model.compile(optimizer = 'rmsprop', loss = 'sparse_categorical_crossentropy', metrics = ['accuracy'])
+
+# preparing the image data 
+train_images = train_images.reshape((60000, 28*28))
+train_images = train_images.astype('float32') / 255
+test_images = test_images.reshape((10000, 28*28))
+test_images = test_images.astype('float32') / 255
+
+# learning
+model.fit(train_images, train_labels, epochs = 5, batch_size = 128)
+
+test_loss, test_acc = model.evaluate(test_images, test_labels)
+print('test_acc: ', test_acc) # about 98%
