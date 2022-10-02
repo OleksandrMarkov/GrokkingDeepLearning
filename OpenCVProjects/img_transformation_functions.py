bd9a20fea114b4d0a7bf61a434dbaedb70148755
Oleksandr Markov <ol.markov1999@gmail.com>
2022-10-02 18:03:42 +0300

    simple image transformation functions

--- OpenCVProjects/img_transformation_functions.py
+++ OpenCVProjects/img_transformation_functions.py
@@ -1,28 +1,42 @@
 import cv2
 import numpy as np
 
-cap = cv2.VideoCapture('videos/road.mp4')
+img = cv2.imread('images/OpenCV.jpg')
 
-#while True:
-while cap.isOpened():
-    success, img = cap.read() 
-    if success == True:	
-	    img = cv2.resize(img,(img.shape[1] // 2, img.shape[0] // 2))
-	    img = cv2.GaussianBlur(img, (9,9), 0)
-	    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-	    
-	    img = cv2.Canny(img, 30, 30)
-	    
-	    kernel = np.ones((5,5), np.uint8)
-	    img = cv2.dilate(img, kernel, iterations=1)
+new_img = np.zeros(img.shape, dtype = 'uint8')
 
-	    img = cv2.erode(img, kernel, iterations=1)
+#img = cv2.flip(img, 1) # mirroring
+# -1 по вертикали и по горизонтали
+# 0 по вертикали  
+# 1 по горизонтали
 
-	    cv2.imshow('Result', img)
-	    
-	    if cv2.waitKey(25) & 0xFF == ord('q'):
-        	break
-    else:
-    	break
+def rotate(img_param, angle):
+	height, width = img_param.shape[:2]
+	point = (width//2, height//2) 
 
-cv2.destroyAllWindows()    
+	mat = cv2.getRotationMatrix2D(point, angle, 1) # zoom scale
+	return cv2.warpAffine(img_param, mat, (width, height))
+
+
+def transform(img_param, x, y):
+	height, width = img_param.shape[:2]
+	mat = np.float32([[1, 0, x], [0, 1, y]])
+	return cv2.warpAffine(img_param, mat, (width, height))
+
+#img = rotate(img, 90)
+#img = transform(img, 30, 200)
+
+img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+img = cv2.GaussianBlur(img, (5,5), 0)
+
+img = cv2.Canny(img, 100, 140) # all colors [0;99] => 0 and [141;255] => 1
+
+# contours and object hierarchy 
+con, hir = cv2.findContours(img, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE) 
+
+cv2.drawContours(new_img, con, -1, (230, 111, 48), 1) # using contours previous img
+# -1 contour id
+# 1 thickness
+
+cv2.imshow("Result", new_img)
+cv2.waitKey(0)
