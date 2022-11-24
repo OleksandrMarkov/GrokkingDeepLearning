ba686e00f1144c61a62d84257b15999ef03b19e6
Oleksandr Markov <ol.markov1999@gmail.com>
2022-11-24 09:37:09 +0200

    Report class created

--- VisitorsFacesDetection/src/VisitorsFacesDetection/package/classes/Alerts.py
+++ VisitorsFacesDetection/src/VisitorsFacesDetection/package/classes/Alerts.py
@@ -1,10 +1,16 @@
 from tkinter import messagebox
 from package.constants import *
 
+import time
+
 class Error:
 	def show(self, message):
 		messagebox.showerror(title = TITLE_ERROR, message = message)
 
 class Info:
 	def show(self, message):
-		messagebox.showinfo(message = message)	
+		messagebox.showinfo(message = message)
+
+class Report():
+	def __init__(self, captured_visitors, snapshots_amount):
+		self.time = time.strftime("%d-%m-%Y-%H-%M-%S")			
